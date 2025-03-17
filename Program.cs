19da8e0c85a47401f90a3287f0899bfde3e60c86
XxNigthslayerxX <rusbeluceta24@gmail.com>
2025-03-17 12:24:54 -0400

    Rehaciendo la tarea
    La api no mostraba un funcionamiento correcto, asi que me dedique a hacer el proyecto de nuevo, trata de un programa de de detenciones policiales

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
+using Tarea7.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
